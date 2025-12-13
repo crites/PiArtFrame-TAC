8ccf33b7c65afd77f13df9fa144bd497d4f376a7
Tom Crites <crites57@gmail.com>
2025-12-13 14:28:52 -0500

    Add comments for clarity in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 
 
-
+# comment added 01/26
 # Set to the name of your e-ink device (https://github.com/robweber/omni-epd#displays-implemented)
 DISPLAY_TYPE = "waveshare_epd.epd7in5_V2"
 
